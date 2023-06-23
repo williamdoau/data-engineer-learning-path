e4527dfbf3e82950c1b51d6feb3e6b920991a00c
william.do.au@gmail.com <william.do.au@gmail.com>
2023-06-23 07:16:39 +0000

    Add code

---
+++ DE 1.2 - Notebook Basics.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+-- MAGIC %python
+-- MAGIC print("I am running Python!")
+
+-- COMMAND ----------
+
+select "I am not using Python"
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC print(f"DA: {DA}")
+-- MAGIC
+-- MAGIC dbutils.fs.ls("/databricks/")
+
+-- COMMAND ----------
+
+
